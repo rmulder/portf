954ec383819b541f8fbd5fb67235cc8c35919e14
Ronan Manoj <ronmantech@gmail.com>
2017-08-02 10:08:22 -0700

    update from basics project

---
+++ basic/watcher.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# to execute: "./watcher.sh &" runs in the background now. 
+
+echo " "
+echo "watching! sccc => css conversion"
+echo "To kill background task: pkill -f watcher.sh"
+echo " "
+
+sass --watch SCSS:CSS
