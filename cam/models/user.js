939918d10cefd82396e2123f4f87d53f6d4c56c5
Ronan Manoj <ronmantech@gmail.com>
2017-08-19 15:50:37 -0700

    updating photo's project

--- cam/models/user.js
+++ cam/models/user.js
@@ -23,6 +23,36 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
+// authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback)
+{
+  User.findOne({ email: email })
+      .exec(function (error, user)
+      {
+        if (error)
+        {
+          return callback(error);
+        }
+        else if ( !user )
+        {
+          var err = new Error('User not found.');
+          err.status = 401;
+          return callback(err);
+        }
+        bcrypt.compare(password, user.password , function(error, result)
+        {
+          if (result === true)
+          {
+            return callback(null, user);
+          }
+          else
+          {
+            return callback();
+          }
+        })
+      });
+}
+
 // hashes password input before storage in DB.
 UserSchema.pre('save', function(next)
 {
--- cam/routes/index.js
+++ cam/routes/index.js
@@ -8,8 +8,31 @@ router.get('/login', function(req, res, next) {
 });
 
 // POST /login
-router.post('/login', function(req, res, next) {
-  return res.send('Logged In!');
+router.post('/login', function(req, res, next)
+{
+  if (req.body.email && req.body.password) // checking if email & password entered
+  {
+    User.authenticate(req.body.email, req.body.password, function (error, user)
+    {
+      if (error || !user)
+      {
+        var err = new Error('Wrong email or password.');
+        err.status = 401; // missing or bad authenticatation
+        return next(err);
+      }
+      else
+      {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  }
+  else
+  {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
 });
 
 
