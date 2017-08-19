f19a005d0ac7098d5a9927d698f2a245f6d28b78
Ronan Manoj <ronmantech@gmail.com>
2017-08-19 11:49:29 -0700

    updating photo's project

--- cam/routes/index.js
+++ cam/routes/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
 
 // GET /register
 router.get('/register', function(req, res, next) {
@@ -8,44 +8,40 @@ router.get('/register', function(req, res, next) {
 });
 
 // POST /register
-router.post('/register', function(req, res, next)
-{
-  if (req.body.email && req.body.name && req.body.favoriteLens &&
+router.post('/register', function(req, res, next) {
+  if (req.body.email &&
+    req.body.name &&
+    req.body.favoriteLens &&
     req.body.password &&
     req.body.confirmPassword)
     {
-      // checks regular password equals confirmed password
+      // confirm that user typed same password twice
       if (req.body.password !== req.body.confirmPassword)
       {
-        var err = new Error("Passwords don't match!");
+        var err = new Error('Passwords do not match.');
         err.status = 400;
         return next(err);
       }
 
       // create object with form input
-      var userData =
-      {
+      var userData = {
         email: req.body.email,
         name: req.body.name,
         favoriteLens: req.body.favoriteLens,
         password: req.body.password
       };
-      // use schema's "create" method to insert document into Mongo
-      User.create(userData, function (error, user)
-      {
-        if (error)
-        {
+      // use schema's `create` method to insert document into Mongo
+      User.create(userData, function (error, user) {
+        if (error) {
           return next(error);
-        }
-        else
-        {
+        } else {
           return res.redirect('/profile');
         }
       });
     }
     else
     {
-      var err = new Error('All fields required!');
+      var err = new Error('All fields required.');
       err.status = 400;
       return next(err);
     }
@@ -59,7 +55,6 @@ router.get('/', function(req, res, next) {
 // GET /about
 router.get('/about', function(req, res, next) {
   return res.render('about', { title: 'About' });
-
 });
 
 // GET /contact
