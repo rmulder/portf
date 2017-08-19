851cf91ccc6092f5f9b23e76d11d47c3e6ef0026
Ronan Manoj <ronmantech@gmail.com>
2017-08-19 12:19:44 -0700

    updating photo's project

--- cam/models/user.js
+++ cam/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
     email: {
       type: String,
@@ -20,6 +21,24 @@ const UserSchema = new mongoose.Schema({
       type: String,
       required: true
     }
+});
+
+// hashes password input before storage in DB. 
+UserSchema.pre('save', function(next)
+{
+  const user = this; // holds user object and it's data.
+
+  // hash and salt.
+  // number = # of times to use encrypt algorithm
+  bcrypt.hash(user.password, 25, function(err, hash)
+  {
+    if (err)
+    {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
 });
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
