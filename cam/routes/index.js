27da43d406a597b53623fc4dd700d98e49685780
Ronan Manoj <ronmantech@gmail.com>
2017-08-19 09:19:41 -0700

    updating photo's project

--- cam/routes/index.js
+++ cam/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 // GET /register
 router.get('/register', function(req, res, next) {
@@ -7,8 +8,47 @@ router.get('/register', function(req, res, next) {
 });
 
 // POST /register
-router.post('/register', function(req, res, next) {
-  return res.send('User created!');
+router.post('/register', function(req, res, next)
+{
+  if (req.body.email && req.body.name && req.body.favoriteLens &&
+    req.body.password &&
+    req.body.confirmPassword)
+    {
+      // checks regular password equals confirmed password
+      if (req.body.password !== req.body.confirmPassword)
+      {
+        var err = new Error("Passwords don't match!");
+        err.status = 400;
+        return next(err);
+      }
+
+      // create object with form input
+      var userData =
+      {
+        email: req.body.email,
+        name: req.body.name,
+        favoriteLens: req.body.favoriteLens,
+        password: req.body.password
+      };
+      // use schema's "create" method to insert document into Mongo
+      User.create(userData, function (error, user)
+      {
+        if (error)
+        {
+          return next(error);
+        }
+        else
+        {
+          return res.redirect('/profile');
+        }
+      });
+    }
+    else
+    {
+      var err = new Error('All fields required!');
+      err.status = 400;
+      return next(err);
+    }
 })
 
 // GET /
