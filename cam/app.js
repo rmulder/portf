ae7f1e045b1cfb3c353a058c4906c0929034f7c0
Ronan Manoj <ronmantech@gmail.com>
2017-08-19 09:10:19 -0700

    updating photo's project

--- cam/app.js
+++ cam/app.js
@@ -1,6 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/photoclub");
+const db = mongoose.connection;
+// errors with database connection, logs to console.
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // parse incoming requests
 app.use(bodyParser.json());
@@ -14,12 +21,12 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
 // include routes
-var routes = require('./routes/index');
+const routes = require('./routes/index');
 app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('File Not Found');
+  const err = new Error('File Not Found');
   err.status = 404;
   next(err);
 });
