7c0a3d9e20381763752f2aea1a129dc7dc08b138
Ronan Manoj <ronmantech@gmail.com>
2017-09-11 11:57:30 -0700

    updating grade's project

--- grade/JS/app.js
+++ grade/JS/app.js
@@ -1,10 +1,37 @@
-'use strict';
+// 'use strict';
 
+calculate = function()
+{
+    let a = document.getElementById('current-percent').value;
+    let b = document.getElementById('min-percent').value;
+    let c = document.getElementById('final-percent').value;
+    let total = parseInt(a)*parseInt(b)*parseInt(c);
+    document.getElementById('total').value = parseInt(a)*parseInt(b)*parseInt(c);
+    console.log("Total of the items entered: " + total);
+}
 
-  let x = 10;
-  let y = 12;
-  let z = 23;
+checker = function()
+{
+  if(total == !NaN)
+  {
+    console.log("Everything was fine when calculating when user entered numbers!");
+  }
+  if( !isNaN(total) )
+  {
+    console.error("There was an error when calculating the numbers entered! Try Again!");
+  }
+}
 
-  let total = (x+y+z);
-
-  console.log("The total sum: " + total);
+// $(document).ready(function()
+// {
+//   function calculate()
+//   {
+//     let curr = $("current-percent").val();
+//     let want = $("min-percent").val();
+//     let final = $("final-percent").val();
+//     let total = curr + want + final;
+//     console.log("The total values added together: " + total);
+//     $("#total").val(total);
+//   }
+//   $('#curr, #want, #final').change(calculate);
+// });
