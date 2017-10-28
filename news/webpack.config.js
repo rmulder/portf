3e7b7d18b819d4a9f0887fb9a0354178b4780f0c
Ronan Manoj <ronmantech@gmail.com>
2017-10-28 02:03:16 -0700

    Updating news api Project

--- news/webpack.config.js
+++ news/webpack.config.js
@@ -1,10 +1,11 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin"),
+ExtractTextPlugin = require("extract-text-webpack-plugin"),
 path = require("path");
 
 module.exports = {
-  entry: ["./src/js/index.js"],
+  entry: ["./js/news.js"],
   output: {
-    filename: "js/index.js",
+    filename: "js/news.js",
     path: path.join(__dirname, "./build/")
   },
   module: {
@@ -19,6 +20,13 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new ExtractTextPlugin({
+      filename: "css/style.css"
     })
   ]
 };
+
+// https://www.valentinog.com/blog/from-gulp-to-webpack-quickstart/ -- webpack
+// https://medium.com/@kimberleycook/intro-to-webpack-1d035a47028d -- intro to webpack
+// https://zellwk.com/blog/bower/ -- bower
