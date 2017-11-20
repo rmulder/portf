7f686416f6291fa6beb42595b86712188db4f789
Ronan Manoj <ronmantech@gmail.com>
2017-11-19 19:27:34 -0800

    updating deter's Project

--- deter/js/app.js
+++ deter/js/app.js
@@ -1,7 +1,30 @@
 "use strict";
 
+console.log("determinate calculator js linked");
+
 function calc()
 {
-   let val [];
-   let sum = 0;
+  let deterStore = []; // array for storage of rows and columns in deter
+
+  deterStore.push(parseInt(document.getElementById('1-enter').value, 10))
+  deterStore.push(parseInt(document.getElementById('2-enter').value, 10))
+  deterStore.push(parseInt(document.getElementById('3-enter').value, 10))
+  deterStore.push(parseInt(document.getElementById('4-enter').value, 10))
+  deterStore.push(parseInt(document.getElementById('5-enter').value, 10))
+  deterStore.push(parseInt(document.getElementById('6-enter').value, 10))
+  deterStore.push(parseInt(document.getElementById('7-enter').value, 10))
+  deterStore.push(parseInt(document.getElementById('8-enter').value, 10))
+  deterStore.push(parseInt(document.getElementById('9-enter').value, 10))
+
+  for(let i = 0; i < deterStore.length; i++)
+  {
+    console.info("Values in determinant:" + deterStore[i]);
+  }
+
+  // Calcuation working
+  const deterFinalVal = ((deterStore[0])*((deterStore[4]*deterStore[8]) - (deterStore[5]*deterStore[7])) - ((deterStore[1])*((deterStore[3]*deterStore[8]) - (deterStore[5]*deterStore[6]))) + ((deterStore[2])*((deterStore[3]*deterStore[7]) - (deterStore[6]*deterStore[4]))) );
+  console.log("Determinant Value: " + deterFinalVal);
+
+  // let pos1 = parseInt(document.getElementById('1-enter').value, 10);
+  // console.log(pos1);
 }
