7d42efa660054f6249f318cdde26cd41ba136b0a
Ronan Manoj <ronmantech@gmail.com>
2017-11-26 15:42:04 -0800

    updating weather's Project

--- grade/gulpfile.js
+++ grade/gulpfile.js
@@ -6,30 +6,41 @@ const gulp = require('gulp'),
   gutil = require('gulp-util'),
   rename = require('gulp-rename'),
   sass = require('gulp-sass'),
-  maps = require('gulp-sourcemaps');
+  maps = require('gulp-sourcemaps'),
+  minhtml = require('gulp-minify-html');
 
-gulp.task("concatScripts", function () {
-
-   gulp.src("scss/style.scss") // js file to be concat
-          .pipe(concat("weather.scss")) // re-name
-          .pipe(maps.write('./'))
-          .pipe(gulp.dest("scss")); // destination of concat js to be located
+gulp.task("minhtml", function () {
+  return gulp.src('index.html')
+  .pipe(minify({
+    minify: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true
+  })).pipe(gulp.dest('./'));
 });
 
-gulp.task("minifyScripts", ["concatScripts"], function() {
+
+gulp.task("concatStyles", function () {
+
+   gulp.src(["scss/vars.scss", "scss/input.scss", "scss/button.scss", "scss/style.scss"]) // scss file to be concat
+          .pipe(concat("totalStylesGrade.scss")) // re-name
+          .pipe(maps.write('./'))
+          .pipe(gulp.dest("scss")); // destination of concat scss to be located
+});
+
+gulp.task("minifyScripts", ["concatStyles"], function() {
 
   return gulp.src('js/app.js')
           .pipe(uglify().on('error', gutil.log)) // if error, outputs to console.
-          .pipe(rename('weather.js'))
+          .pipe(rename('finalgrade.js'))
           .pipe(gulp.dest('js'));
 });
 
 gulp.task("compileSass", function() {
 
   // main stylesheet:
-   gulp.src("scss/style.scss")
+   gulp.src("scss/totalStylesGrade.scss")
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(rename('weather.css'))
+    .pipe(rename('final.css'))
     .pipe(maps.write('./')) // sourcemaps to css
     .pipe(gulp.dest('css'))
 });
