80fc0733d5b88058711ab3a466f7184086dcdacb
Ronan Manoj <ronmantech@gmail.com>
2017-12-09 23:03:31 -0800

    updating checkin's project

---
+++ checkin/models/user.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const UserSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    favoriteLens: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+});
+// authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({ email: email })
+      .exec(function (error, user)
+      {
+        if (error)
+        {
+          return callback(error);
+        }
+        else if ( !user )
+        {
+          var err = new Error('User not found.');
+          err.status = 401;
+          return callback(err);
+        }
+        bcrypt.compare(password, user.password , function(error, result)
+        {
+          if (result === true)
+          {
+            return callback(null, user);
+          }
+          else
+          {
+            return callback();
+          }
+        })
+      });
+}
+// hash password before saving to database
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 3, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
---
+++ checkin/routes/index.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let router = express.Router();
+let User = require('../models/user');
+// let mid = require('../middleware');
+
+
+// GET - Home page
+router.get('/', function(req, res, next)
+{
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /create
+router.get('/create', function(req, res, next)
+{
+  return res.render('create', { title: 'Booking Flight' });
+});
+
+// GET /checkin
+router.get('/checkin', function(req, res, next)
+{
+  return res.render('checkin', { title: 'Checking-In' });
+});
+
+
+// GET /result
+router.get('/result', function(req, res, next)
+{
+  return res.render('result', { title: 'Result' });
+});
+
+
+module.exports = router;
