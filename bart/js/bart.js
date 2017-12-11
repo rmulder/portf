572dc1b235cefd394f81df2ad511282138a6fc66
Ronan Manoj <ronmantech@gmail.com>
2017-12-11 12:04:16 -0800

    updating bart's project

--- bart/js/bart.js
+++ bart/js/bart.js
@@ -2,7 +2,7 @@
 
 console.info("Inside bart.js");
 
-let fremontCall = () =>
+let overallCall = () =>
 {
   // example:
   // http://api.bart.gov/api/etd.aspx?cmd=etd&orig=FRMT&key=MW9S-E7SL-26DU-VV8V&json=y
@@ -19,22 +19,32 @@ let fremontCall = () =>
     localTime = localTime.substr(1).slice(0, -1); // remove the quote marks
     console.info("Local Time: " + localTime);
 
-    let firstDestinName = (JSON.stringify(json.root.station[0].etd[0].destination))
-    firstDestinName = firstDestinName.substr(1).slice(0, -1); // remove the quote marks
-    console.info("1 Destination: " + firstDestinName);
-
-    let firstDepTime = (JSON.stringify(json.root.station[0].etd[0].estimate[0].minutes))
-    // firstDepTime = firstDepTime.substr(1).slice(0, -1); // remove the quote marks
-    console.info("1 dest. Depart Time: " + firstDepTime);
-
-    let firstplatform = (JSON.stringify(json.root.station[0].etd[0].estimate[0].platform))
-    firstplatform = firstplatform.substr(1).slice(0, -1); // remove the quote marks
-    console.info("1 dest. Platform Number: " + firstplatform);
-
-    let firstcolor = (JSON.stringify(json.root.station[0].etd[0].estimate[0].color))
-    firstcolor = firstcolor.substr(1).slice(0, -1); // remove the quote marks
-    console.info("Train Color: " + firstcolor);
+    firstCall(json);
 
   });
 }
-fremontCall();
+overallCall();
+
+let firstCall = (firstjson) =>
+{
+  let firstDestinName = (JSON.stringify(firstjson.root.station[0].etd[0].destination))
+  firstDestinName = firstDestinName.substr(1).slice(0, -1); // remove the quote marks
+  console.info("1 Destination: " + firstDestinName);
+  //
+  // let firstDestinName = (JSON.stringify(firstjson.root.station[0].etd[0].destination))
+  // firstDestinName = firstDestinName.substr(1).slice(0, -1); // remove the quote marks
+  // console.info("1 Destination: " + firstDestinName);
+
+  let firstDepTime = (JSON.stringify(firstjson.root.station[0].etd[0].estimate[0].minutes))
+  firstDepTime = firstDepTime.substr(1).slice(0, -1); // remove the quote marks
+  console.info("1 dest. Depart Time: " + firstDepTime);
+
+  let firstplatform = (JSON.stringify(firstjson.root.station[0].etd[0].estimate[0].platform))
+  firstplatform = firstplatform.substr(1).slice(0, -1); // remove the quote marks
+  console.info("1 dest. Platform Number: " + firstplatform);
+
+  let firstcolor = (JSON.stringify(firstjson.root.station[0].etd[0].estimate[0].color))
+  firstcolor = firstcolor.substr(1).slice(0, -1); // remove the quote marks
+  console.info("Train Color: " + firstcolor);
+}
+// firstCall();
