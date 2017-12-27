7fecc048c2ef6d6765deb434675f7ebab0ec5262
Ronan Manoj <ronmantech@gmail.com>
2017-12-27 09:24:29 -0800

    updating cam's project

--- cam/app.js
+++ cam/app.js
@@ -21,7 +21,7 @@ app.use(session({
   })
 }));
 
-// make user ID available in templates
+// user ID available in templates
 app.use(function (req, res, next) {
   res.locals.currentUser = req.session.userId;
   next();
