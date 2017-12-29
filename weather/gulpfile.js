97fe3f161d32ab4ee7ee9248a3b9f3b46193689e
Ronan Manoj <ronmantech@gmail.com>
2017-12-28 17:01:58 -0800

    updating weather's Project

--- weather/gulpfile.js
+++ weather/gulpfile.js
@@ -2,38 +2,44 @@
 
 const gulp = require('gulp'),
   concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
+  uglify = require('gulp-uglifyes'),
+  htmlmin = require('gulp-htmlmin'),
   gutil = require('gulp-util'),
   rename = require('gulp-rename'),
   sass = require('gulp-sass'),
   maps = require('gulp-sourcemaps');
 
-gulp.task("concatScripts", function () {
-
-   gulp.src("scss/style.scss") // js file to be concat
+gulp.task("concatScripts", function ()
+{
+   gulp.src("src/scss/style.scss") // js file to be concat
           .pipe(concat("weather.scss")) // re-name
-          .pipe(maps.write('./'))
-          .pipe(gulp.dest("scss")); // destination of concat js to be located
+          .pipe(gulp.dest("buid/scss")); // destination of concat js to be located
 });
 
-gulp.task("minifyScripts", ["concatScripts"], function() {
+gulp.task('htmlcompress', function() {
+    return gulp.src('src/index.html')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(rename('index.min.html'))
+      .pipe(gulp.dest('build/html'));
+  });
 
-  return gulp.src('js/app.js')
+// ES6 min. support
+gulp.task("minifyScripts", ["concatScripts"], function()
+{
+  return gulp.src('src/js/app.js')
           .pipe(uglify().on('error', gutil.log)) // if error, outputs to console.
           .pipe(rename('weather.js'))
-          .pipe(gulp.dest('js'));
+          .pipe(gulp.dest('build/js'));
 });
 
-gulp.task("compileSass", function() {
-
-  // main stylesheet:
-   gulp.src("scss/style.scss")
+gulp.task("compileSass", function()
+{
+   gulp.src("src/scss/style.scss")
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(rename('weather.css'))
-    .pipe(maps.write('./')) // sourcemaps to css
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('build/css'))
 });
 
-gulp.task("build", ['minifyScripts', 'compileSass']);
+gulp.task("build", ['minifyScripts', 'compileSass', 'htmlcompress']);
 
 gulp.task("default", ['build']);
