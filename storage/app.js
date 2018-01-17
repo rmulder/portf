fecf046444b6acd9b7b83b448af95c5dc26be37d
Ronan Manoj <ronmantech@gmail.com>
2018-01-17 15:56:52 -0800

    updating storage's Project

--- storage/app.js
+++ storage/app.js
@@ -1,12 +1,21 @@
 const express = require('express'),
   bodyParser = require('body-parser'),
+  mongoose = require("mongoose"),
   app = express();
 
 // server on port 8080 or enviroment variable defined when hosting
 const PORT = process.env.PORT || 8080;
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/storage");
+let nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+let User = mongoose.model("User", nameSchema);
+
 app.use(bodyParser.json()); // parse incoming requests
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // static files link from /public
 app.use(express.static(__dirname + '/public'));
@@ -17,22 +26,15 @@ app.set('views', __dirname + '/views');
 let routes = require('./routes/index'); // include routes
 app.use('/', routes);
 
-// catch 404 then forward to error handler
-app.use(function(req, res, next)
-{
-  let err = new Error('404, File Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// last app.use callback
-app.use(function(err, req, res, next)
-{
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
 });
 
 app.listen(PORT, () => {
--- storage/routes/index.js
+++ storage/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET /(index)
 router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
+  return res.render('index', { title: 'Index' });
 });
 
 module.exports = router;
