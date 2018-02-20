f0e67610336ccc0674595a56421e5be803b27efd
Ronan Manoj <ronmantech@gmail.com>
2018-02-20 13:17:36 -0800

    updated chat files.

--- chat/app.js
+++ chat/app.js
@@ -4,16 +4,19 @@ const app = require('express')(),
 
 const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.get('/', function(req, res)
+{
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket)
+{
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-http.listen(port, function(){
+http.listen(port, function()
+{
   console.log('listening on *:' + port);
 });
