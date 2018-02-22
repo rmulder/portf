df604e79e4a87dda681d89df7bef8e38c54b2a9c
Ronan Manoj <ronmantech@gmail.com>
2018-02-22 09:40:00 -0800

    updated.

--- webCrawler/app.js
+++ webCrawler/app.js
@@ -2,22 +2,35 @@ const request = require('request');
 const cheerio = require('url-parse');
 const url-parse = require('url-parse');
 
+const START_URL = "http://www.arstechnica.com";
+const SEARCH_WORD = "stemming";
+const MAX_PAGES_TO_VISIT = 10;
 
-let pageToVisit = "http://www.arstechnica.com";
+let pagesVisited = {};
+let numPagesVisited = 0;
+let pagesToVisit = [];
+let url = new URL(START_URL);
+let baseUrl = url.protocol + "//" + url.hostname;
 
-console.log("Visiting page " + pageToVisit);
-request(pageToVisit, function(error, response, body)
+pagesToVisit.push(START_URL);
+crawl();
+
+function crawl()
 {
-   if(error)
-   {
-     console.error("Error: " + error);
-   }
-   // Check status code (200 is HTTP OK)
-   console.log("Status code: " + response.statusCode);
-   if(response.statusCode === 200)
-   {
-     // Parse the document body
-     let $ = cheerio.load(body);
-     console.log("Page title:  " + $('title').text());
-   }
-});
+  if(numPagesVisited >= MAX_PAGES_TO_VISIT)
+  {
+    console.log("Reached max limit of number of pages to visit.");
+    return;
+  }
+  let nextPage = pagesToVisit.pop();
+  if (nextPage in pagesVisited)
+  {
+    // We've already visited this page, so repeat the crawl
+    crawl();
+  }
+  else
+  {
+    // New page we haven't visited
+    visitPage(nextPage, crawl);
+  }
+}
