f3a0146647288411db8e945084f1bc3c126f17d1
Ronan Manoj <ronmantech@gmail.com>
2018-04-08 13:28:15 -0700

    updating weather's Project

--- weather/tools/gulpfile.js
+++ weather/tools/gulpfile.js
@@ -10,21 +10,21 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const inject = require('gulp-inject-string');
 
-gulp.task("concatScripts", function()
+gulp.task("concatScripts", () =>
 {
    gulp.src("../src/scss/style.scss")
     .pipe(concat("weather.scss")) // re-name
     .pipe(gulp.dest("../src/scss"));
 });
 
-gulp.task('imgCompress', function()
+gulp.task('imgCompress', () =>
 {
   gulp.src('../src/img/favicon.img')
    .pipe(imagemin({ optimizationLevel: 5 }))
    .pipe(gulp.dest('../build/img'))
 });
 
-gulp.task('htmlcompress', function()
+gulp.task('htmlcompress', () =>
 {
   return gulp.src('../src/index.html')
     .pipe(inject.replace('app.js', 'weather.min.js'))
@@ -35,7 +35,7 @@ gulp.task('htmlcompress', function()
   });
 
 // ES6 min. support
-gulp.task("minifyScripts", ["concatScripts"], function()
+gulp.task("minifyScripts", ["concatScripts"], () =>
 {
   return gulp.src('../src/js/app.js')
     .pipe(uglify().on('error', gutil.log)) // if error, outputs to console.
@@ -43,7 +43,7 @@ gulp.task("minifyScripts", ["concatScripts"], function()
     .pipe(gulp.dest('../build/js'));
 });
 
-gulp.task("compileSass", function()
+gulp.task("compileSass", () =>
 {
    gulp.src("../src/scss/weather.scss")
     .pipe(sass({outputStyle: 'compressed'}))
