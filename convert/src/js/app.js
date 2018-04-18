323e32334114571df1a2b9e46b5e08c99f5f7aa5
Ronan Manoj <ronmantech@gmail.com>
2018-04-18 14:43:24 -0700

    updating convert's Project

--- convert/src/js/app.js
+++ convert/src/js/app.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const checker = () =>
+{
+  if((regex1.test(input2) == true))
+  {
+    console.log("Binary number!");
+  }
+  else if (regex1.test(input2) == false)
+  {
+    console.log("Not a binary number");
+  }
+  else
+  {
+    console.error("error!");
+  }
+}
+
+let input = "23";
+let input2 = "1101000";
+
+let regex1 = RegExp("[01]+");
+
+if((regex1.test(input2) == true))
+{
+  console.log("Binary number!");
+}
+else
+{
+  console.log("Not a binary number");
+}
