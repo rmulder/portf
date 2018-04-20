c67b4ec75ffebcb1abc1c2f220a4a53dbb364aa1
Ronan Manoj <ronmantech@gmail.com>
2018-04-19 22:18:54 -0700

    updating joiner's Project

---
+++ joiner/README.md
@@ -0,0 +1,11 @@
+# Joiner 
+Merges PDFs together. 
+Used often in Engineering courses
+
+## Technologies Used
+* HTML
+* CSS
+* Mocha
+* Chai
+* ES6
+* Gulp.js
---
+++ joiner/app.js
@@ -0,0 +1,10 @@
+var merge = require('easy-pdf-merge');
+
+merge(['/Users/ronanmanoj/Desktop/print_check_list.pdf','/Users/ronanmanoj/Desktop/RonanCoverLetter.pdf'],'/Users/ronanmanoj/Desktop/OUTPUT.pdf',function(err){
+ 
+        if(err)
+        return console.log(err);
+ 
+        console.log('Successfully merged!');
+ 
+});
