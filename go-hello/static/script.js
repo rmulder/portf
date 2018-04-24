6960008cf059cef5661cc4119874c8a7c0c59604
Ronan Manoj <ronmantech@gmail.com>
2018-04-23 23:38:49 -0700

    update.

---
+++ go-hello/static/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function fetchMessage() 
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "/hello", false);
+    xmlHttp.send(null);
+    document.getElementById("message").innerHTML = xmlHttp.responseText;
+}
