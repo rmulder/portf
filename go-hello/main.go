0634de6c0c3d3b9904fa77935087123eb129ca0b
Ronan Manoj <ronmantech@gmail.com>
2018-04-25 21:27:27 -0700

    update.

--- go-hello/main.go
+++ go-hello/main.go
@@ -3,7 +3,6 @@ package main
 import
 (
 	"github.com/kataras/iris"
-
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
 )
@@ -11,33 +10,14 @@ import
 func main() {
 	app := iris.New()
 	app.Logger().SetLevel("debug")
-	// Optionally, add two built'n handlers
-	// that can recover from any http-relative panics
-	// and log the requests to the terminal.
 	app.Use(recover.New())
 	app.Use(logger.New())
 
-	// Method:   GET
-	// Resource: http://localhost:8080
 	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.HTML("<h1>Welcome</h1>")
+		ctx.HTML("<p>Hello, World!</p>")
+		// or: ctx.WriteString("Hello, World!")
 	})
 
-	// same as app.Handle("GET", "/ping", [...])
-	// Method:   GET
-	// Resource: http://localhost:8080/ping
-	app.Get("/ping", func(ctx iris.Context) {
-		ctx.WriteString("pong")
-	})
-
-	// Method:   GET
-	// Resource: http://localhost:8080/hello
-	app.Get("/hello", func(ctx iris.Context) {
-		ctx.JSON(iris.Map{"message": "Hello Iris!"})
-	})
-
-	// http://localhost:8080
-	// http://localhost:8080/ping
-	// http://localhost:8080/hello
+	// localhost:8080
 	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
