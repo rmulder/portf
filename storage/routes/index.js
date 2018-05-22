ebb5b099c7e5152f2d27beb3ae2f01fa665a1059
Ronan Manoj <ronmantech@gmail.com>
2018-05-22 16:49:16 -0700

    updating storage's Project

--- storage/routes/index.js
+++ storage/routes/index.js
@@ -14,7 +14,7 @@ let Names = mongoose.model("Names", nameSchema);
 // GET /(index)
 router.get('/', (req, res, next) =>
 {
-  return res.render('index', { title: 'Index' });
+  return res.render('index', { title: 'Home' });
 });
 
 router.post("/addname", (req, res) =>
@@ -25,13 +25,46 @@ router.post("/addname", (req, res) =>
     {
       res.redirect(301, '/');
       console.log("Item added to database!");
-      console.log(item);
+      console.log("New User db id: ", item._id);
+      console.log("All information: ", item);
     })
     .catch(err =>
       {
         console.error("Can't save to database!");
         res.status(400).send("Unable to save to database");
       });
+});
+
+router.post('/find', (req, res) =>
+{
+  let id = req.body.id;
+
+  UserData.findById(id, (err, doc) =>
+  {
+    if (err)
+    {
+      console.error('error, no entry found');
+    }
+    doc.title = req.body.title;
+    doc.content = req.body.content;
+    doc.author = req.body.author;
+    doc.save();
+  })
+  res.render('return', { firstName: 'id.firstName' });
+
+
+  // let myData = new Names(req.body);
+  // console.log("The request id: ", req.body);
+  // myData.find()
+  //   .then(item =>
+  //     {
+  //       return res.render('return', {firstName: "somo", title: 'Search'});
+  //     })
+  //   .catch(err =>
+  //     {
+  //       console.error("Can't load info from database!");
+  //       res.status(405).send("Unable to load data from database");
+  //     });
 });
 
 // router.get('/find', (req, res, next) =>
