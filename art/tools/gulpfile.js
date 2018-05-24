bc361a4d1834af7f3aa8693ea5eb360beff6b153
Ronan Manoj <ronmantech@gmail.com>
2018-05-23 22:58:12 -0700

    updating art's project

--- art/tools/gulpfile.js
+++ art/tools/gulpfile.js
@@ -1,66 +1,79 @@
 "use strict";
 
-const gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglifyes'),
-  htmlmin = require('gulp-htmlmin'),
-  imagemin = require('gulp-imagemin'),
-  gulpSequence = require('gulp-sequence'),
-  gutil = require('gulp-util'),
-  rename = require('gulp-rename'),
-  sass = require('gulp-sass');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglifyes');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const gulpSequence = require('gulp-sequence');
+const gutil = require('gulp-util');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const inject = require('gulp-inject-string');
 
-gulp.task('htmlcompress', function()
+const paths =
 {
-  gulp.src('../src/index.html')
+  scripts: ['../src/JS/*.js', '!../src/JS/script.js'],
+  scss: ['../src/SCSS/style.scss', '../src/SCSS/gallery.scss', '!../src/SCSS/vars.scss'],
+  images: '../src/img/*',
+  html: ['../src/index.html', '../src/photos.html'],
+  css: ['../src/CSS/*.css', '!../src/CSS/*.map', '!../src/CSS/vars.css']
+};
+
+gulp.task('htmlcompress', () =>
+{
+  gulp.src(paths.html[0])
+    .pipe(inject.replace('CSS/style.css', 'css/style.min.css'))
+    .pipe(inject.replace('<link rel = "stylesheet" type="text/css" href="CSS/var.css">', '')) //removal
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(rename('index.min.html'))
     .pipe(gulp.dest('../build'));
 
-  gulp.src('../src/HTML/photos.html')
+  gulp.src(paths.html[1])
+    .pipe(inject.replace('CSS/gallery.css', 'css/gallery.min.css'))
+    .pipe(inject.replace('CSS/gallery.css', 'css/gallery.min.css'))
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(rename('photos.min.html'))
-    .pipe(gulp.dest('../build/html'));
+    .pipe(gulp.dest('../build'));
 });
 
-gulp.task('imgCompress', function()
+gulp.task('imgCompress', () =>
 {
-  gulp.src('../src/IMG/*')
-    .pipe(imagemin({ optimizationLevel: 5 }))
+  gulp.src(paths.images)
+    .pipe(imagemin({ optimizationLevel: 7 }))
     .pipe(gulp.dest('../build/img'))
 });
 
-gulp.task("concatStyle", function ()
+gulp.task("concatStyle", () =>
 {
-  gulp.src(["../src/scss/style.scss"])
+  gulp.src(paths.scss[0])
     .pipe(concat("style.scss"))
     .pipe(gulp.dest("../src/scss"));
 
-  gulp.src(["../src/scss/gallery.scss"])
+  gulp.src(paths.scss[1])
     .pipe(concat("gallery.scss"))
     .pipe(gulp.dest("../src/scss"));
 });
 
-// gulp.task("jscompress", function()
-// {
-//    gulp.src('../src/js/app.js')
-//       .pipe(uglify().on('error', gutil.log)) // error outputs to console.
-//       // .pipe(uglify())
-//       .pipe(rename('deter.min.js'))
-//       .pipe(gulp.dest('../build/js'));
-// });
-
-gulp.task("compileSass", function()
+gulp.task("compileSass", () =>
 {
-  gulp.src("../src/scss/style.scss")
+  gulp.src(paths.scss[0])
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('../build/css'))
 
-  gulp.src("../src/scss/gallery.scss")
+  gulp.src(paths.scss[1])
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(rename('gallery.min.css'))
     .pipe(gulp.dest('../build/css'))
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', () =>
+{
+  gulp.watch(paths.scss, ['compileSass']);
+  gulp.watch(paths.images, ['imgCompress']);
+  gulp.watch(paths.html, ['htmlcompress']);
 });
 
 gulp.task("build", gulpSequence(['htmlcompress', 'concatStyle', 'imgCompress'], 'compileSass'));
