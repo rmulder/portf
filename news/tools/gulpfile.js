710aba3e1c5d67b22479831d53c9f7696fce9d55
Ronan Manoj <ronmantech@gmail.com>
2018-05-25 14:31:52 -0700

    updating news api Project

--- news/tools/gulpfile.js
+++ news/tools/gulpfile.js
@@ -11,49 +11,72 @@ const gulpSequence = require('gulp-sequence');
 const sass = require('gulp-sass');
 const inject = require('gulp-inject-string');
 
+const paths = {
+  scss: {
+    src: ['../src/scss/*.scss', '!../src/scss/vars.scss', '!../src/scss/news.scss'],
+    spec: '../src/scss/news.scss',
+    dest: '../src/scss'
+  },
+  css: {
+    dest: '../build/css'
+  },
+  js: {
+    src: '../src/js/*.js',
+    dest: '../build/js'
+  },
+  html: {
+    src: '../src/index.html',
+    dest: '../build'
+  },
+  img: {
+    src: '../src/img/**',
+    dest: '../build/img'
+  }
+};
+
 gulp.task("concatStyles", () => 
 {
-  gulp.src(["../src/scss/cards.scss", "../src/scss/style.scss", "../src/scss/boxes.scss", "../src/scss/vars.scss"])
+  gulp.src(paths.scss.src)
       .pipe(concat("news.scss"))
-      .pipe(gulp.dest("../src/scss"));
+      .pipe(gulp.dest(paths.scss.dest));
 });
 
 gulp.task('imgCompress', () =>
 {
-  gulp.src('../src/img/favicon.ico')
+  gulp.src(paths.img.src)
        .pipe(imagemin({ optimizationLevel: 5 }))
-       .pipe(gulp.dest('../build/img'))
+       .pipe(gulp.dest(paths.img.dest))
 });
 
 
 // inject.replace inside
 gulp.task('htmlcompress', () =>
 {
-  gulp.src('../src/index.html')
+  gulp.src(paths.html.src)
       .pipe(inject.replace('news.js', 'news.min.js'))
       .pipe(inject.replace('style.css', 'news.min.css'))
       .pipe(inject.replace('<link rel = "stylesheet" href="css/cards.css">', '')) //removal
       .pipe(inject.replace('<link rel = "stylesheet" href="css/boxes.css">', '')) //removal
       .pipe(htmlmin({collapseWhitespace: true}))
       .pipe(rename('index.min.html'))
-      .pipe(gulp.dest('../build'));
+      .pipe(gulp.dest(paths.html.dest));
   });
 
 // ES6 min. support
 gulp.task("minifyScripts", () =>
-		{
-  gulp.src('../src/js/news.js')
+{
+  gulp.src(paths.js.src)
       .pipe(uglify().on('error', gutil.log)) // if error, outputs to console.
       .pipe(rename('news.min.js'))
-      .pipe(gulp.dest('../build/js'));
+      .pipe(gulp.dest(paths.js.dest));
 });
 
 gulp.task("compileSass", () =>
 {
-  gulp.src("../src/scss/news.scss")
+  gulp.src(paths.scss.spec)
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(rename('news.min.css'))
-    .pipe(gulp.dest('../build/css'))
+    .pipe(gulp.dest(paths.css.dest))
 });
 
 gulp.task("build", gulpSequence(['concatStyles', 'imgCompress', 'htmlcompress', 'minifyScripts'], 'compileSass'));
