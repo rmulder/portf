00412bba6b03c2706a4a5257c71e7acff556aa12
Ronan Manoj <ronmantech@gmail.com>
2018-06-07 09:39:52 -0700

    errorhandlingupdate

--- sandbox_test/httpsTest/2app.js
+++ sandbox_test/httpsTest/2app.js
@@ -9,27 +9,29 @@ const helmet = require('helmet');
 const app = express();
 
 app.use(helmet());
-
 app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
     styleSrc: ["'self'",'ajax.googleapis.com']
   }
 }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 8080 || process.env.PORT;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
 
 const indexRouter = require('./routes/index');
 //const usersRouter = require('./routes/users');
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 const certOptions = {
   key: fs.readFileSync(path.resolve('../../../../server.key')),
@@ -40,4 +42,22 @@ const server = https.createServer(certOptions, app).listen(8080);
 childProc.exec(`open -a "Google Chrome" https://localhost:${port}`, () =>
 {
  console.log(`On: https://localhost:${port}`); 
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) =>
+{
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) =>
+{
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
--- sandbox_test/httpsTest/routes/index.js
+++ sandbox_test/httpsTest/routes/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) =>
 {
- res.send("Hello, HTTPS World");
- // res.render('index', { title: 'Express' });
+// res.send("Hello, HTTPS World");
+  res.render('index', { title: 'HTTPS World' });
 });
 
 module.exports = router;
