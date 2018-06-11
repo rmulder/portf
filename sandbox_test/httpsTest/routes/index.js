dd08e67b27c37a1354c2fb0fea82220d739ff231
Ronan Manoj <ronmantech@gmail.com>
2018-06-10 17:20:34 -0700

    update

--- sandbox_test/httpsTest/routes/index.js
+++ sandbox_test/httpsTest/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) =>
+router.get('*', (req, res, next) =>
 {
 // res.send("Hello, HTTPS World");
   res.render('index', { title: 'HTTPS World' });
